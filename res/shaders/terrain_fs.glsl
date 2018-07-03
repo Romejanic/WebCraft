bcae5c3fd38fc70715286f7baaf2e1ab8694f106
Romejanic <romejanic@gmail.com>
2018-07-03 22:58:16 +1000

    Basic lighting

--- res/shaders/terrain_fs.glsl
+++ res/shaders/terrain_fs.glsl
@@ -5,6 +5,12 @@ varying vec3 v_normal;
 
 uniform sampler2D atlas;
 
+const vec3 lightDirection = normalize(vec3(45.,30.,-45.));
+
 void main() {
+    vec3 normal = normalize(v_normal);
+    float ndotl = max(dot(lightDirection, normal),.2);
+
     gl_FragColor = texture2D(atlas, v_texCoords);
+    gl_FragColor.xyz *= ndotl;
 }
